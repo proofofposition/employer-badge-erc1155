4963d4695c3eef33b6769ff4b25f90d54f281ce4
Gareth Edwards <garrethedwards@gmail.com>
2022-09-07 16:19:34 +0200

    Add deploy script and dotenv support

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,16 @@
 require("@nomicfoundation/hardhat-toolbox");
-require("hardhat-tracer");
+require('dotenv').config({path:__dirname+'/.env'})
+const { API_URL, PRIVATE_KEY } = process.env;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.9",
+  networks: {
+    rinkeby: {
+      url: API_URL,
+      accounts: [
+        PRIVATE_KEY,
+      ],
+    },
+  },
 };
