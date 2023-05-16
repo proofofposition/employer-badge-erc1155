a9a2aefb390d6d850bb9e58dfd52459423f34912
Gareth Edwards <garrethedwards@gmail.com>
2023-05-16 20:12:44 +0200

    improve network configs

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,35 +1,41 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("hardhat-gas-reporter");
 require("solidity-coverage");
-require('dotenv').config({path:__dirname+'/.env'})
-const { SEPOLIA_API_URL, GOERLI_API_URL, PRIVATE_KEY, REPORT_GAS } = process.env;
+require('dotenv').config({path: __dirname + '/.env'})
+const {OPTIMISM_GOERLI_API_URL, SEPOLIA_API_URL, GOERLI_API_URL, PRIVATE_KEY, REPORT_GAS} = process.env;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: {
-    version: "0.8.17",
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 200
-      }
-    }
-  },
-  networks: {
-    goerli: {
-      url: GOERLI_API_URL,
-      accounts: [
-        PRIVATE_KEY,
-      ],
+    solidity: {
+        version: "0.8.17",
+        settings: {
+            optimizer: {
+                enabled: true,
+                runs: 200
+            }
+        }
     },
-    sepolia: {
-      url: SEPOLIA_API_URL,
-      accounts: [
-        PRIVATE_KEY,
-      ],
+    networks: {
+        goerli: {
+            url: GOERLI_API_URL,
+            accounts: [
+                PRIVATE_KEY
+            ]
+        },
+        optimism_goerli: {
+            url: OPTIMISM_GOERLI_API_URL,
+            accounts: [
+                PRIVATE_KEY
+            ]
+        },
+        sepolia: {
+            url: SEPOLIA_API_URL,
+            accounts: [
+                PRIVATE_KEY
+            ]
+        }
+    },
+    gasReporter: {
+        enabled: !!(REPORT_GAS)
     }
-  },
-  gasReporter: {
-    enabled: !!(REPORT_GAS)
-  }
 };
